34d6f4701b61d2f9be31b938fdc4abf7d3036e91
noppelmax <max@noppelmax.online>
2020-09-02 10:10:01 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# ImageOpt
+# Implicationtracker
 
-## Berechne Näherungen an ein Bild mit Dreiecken / Vierecken
+This tool does calculate inconsistencies and incompletenesses of a hierarchy of notions. It reads in a `.im`, which includes a list of implications.
++ `=>`
++ `<=`
++ `<=>`
++ `=|=>`
++ `<=|=`
+
+Based on this input it generates the complete adjacent matrix. Therefore is applies the transitivity of `=>` and the antitransitivity of `=|=>`. Additional it includes an LaTex export, s.t. it outputs longtables for `pages` of notions. 
+
+## Usage / Installation
+
+## `.im` format parser
+
+## Additional functionality and special notions
+
+## Testsets
+
+## Licence
+Copyright (c) 2020 Maximilian Noppel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 # Author: Maximilian Noppel
 # July 2020
 #
-# Credits to: https://www.techiedelight.com/kahn-topological-sort-algorithm/
-# The implementation of Kahn Alg. is from this website.
-#
+
 
 import logging
 from collections import deque
@@ -340,65 +338,6 @@ else:
 
 
 
-
-# class to represent a graph object:
-class Graph:
-
-	# stores indegree of a vertex
-	indegree = None
-
-	# Constructor
-	def __init__(self, edges, N):
-		# A List of Lists to represent an adjacency list
-		self.adjList = [[] for _ in range(N)]
-		# initialize indegree of each vertex by 0
-		self.indegree = [0] * N
-		# add edges to the undirected graph
-		for (src, dest) in edges:
-			# add an edge from source to destination
-			self.adjList[src].append(dest)
-			# increment in-degree of destination vertex by 1
-			self.indegree[dest] = self.indegree[dest] + 1
-
-
-# performs Topological Sort on a given DAG
-def doTopologicalSort(graph, N):
-
-	# list to store the sorted elements
-	L = []
-
-	# get in-degree information of the graph
-	indegree = graph.indegree
-
-	# Set of all nodes with no incoming edges
-	S = deque([i for i in range(N) if indegree[i] == 0])
-
-	while S:
-		# remove a node n from S
-		n = S.pop()
-		# add n to tail of L
-		L.append(n)
-		for m in graph.adjList[n]:
-			# remove edge from n to m from the graph
-			indegree[m] = indegree[m] - 1
-			# if m has no other incoming edges then
-			# insert m into S
-			if indegree[m] == 0:
-				S.append(m)
-
-	# if graph has edges then graph has at least one cycle
-	for i in range(N):
-		if indegree[i]:
-			return None
-
-	return L
-
-
-def substituteNames(name):
-	pass
-
-
-
 def getImplications(implicationsfile,notions):
 	edges = []
 	nonEdges = []
@@ -691,26 +630,7 @@ if __name__ == '__main__':
 
 	N = len(notions)
 
-	graph = Graph(edges, N)
-	L = doTopologicalSort(graph, N)
 
-	if L:
-		#for i in L:
-		#	print(notions[i])
-		pass
-	else:
-		#logger.info("Graph has at least one cycle. Topological sorting is not possible.")
-		#exit(0)
-		pass
-
-	# No cycles so far!
-	# Checking the disproved implications
-	#if checkDisprovedImplications(edges,nonEdges,notions) == False:
-	#	logger.info("Conflicts with disproved implications")
-		#exit(0)
-
-
-	
 	originalMatrix = [[ False for _ in range(len(notions)) ] for _ in range(len(notions))]
 	for (src, dest) in edges:
 		originalMatrix[src][dest] = True
